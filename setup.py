42d237e8c86b0af5bce55634c7d06fac905c56d9
Michael Van Treeck <michael.vantreeck@mq.edu.au>
2017-09-01 09:58:37 +1000

    Added setup files for pip installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='piano_utils',
+    modules=['piano_utils'],
+    version='0.1',
+    description='Piano Utilities',
+    author='Michael Van Treeck',
+    author_email='michael.vantreeck@mq.edu.au',
+    url='https://github.com/mvt147/PianoUtils.git',
+    keywords=['piano', 'util', 'xml', 'json'],
+    classifiers=[],
+)
